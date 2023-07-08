37520559aebf244cb303fce7d82307411b98724a
Anton <antonilic7@gmail.com>
2023-07-07 20:33:19 -0700

    added code to change scenes

--- JamGame/Assets/Scripts/changeScene.cs
+++ JamGame/Assets/Scripts/changeScene.cs
@@ -1,1 +1,25 @@
 using UnityEngine;
+
+public class SwitchBackground : MonoBehavior
+{
+    private SpriteRenderer spriteRenderer;
+    //public string background1Path;
+    //public string background2Path;
+
+    void Start()
+    {
+
+    }
+
+    void Update(
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if statements to load different backgrounds - if input is A.. if input is B ... etc.
+            
+            //spriteRenderer.sprite = Resources.Load<Sprite>(background1Path);
+            //spriteRenderer.sprite = Resources.Load<Sprite>(background2Path);
+        }
+    }
+    )
+}
